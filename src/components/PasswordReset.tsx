aca44f719b388b3859e17689c4f509f7d4e8c8db
Ricky4412 <124883161+Ricky4412@users.noreply.github.com>
2025-02-07 23:35:21 +0000

    Update PasswordReset.tsx

--- src/components/PasswordReset.tsx
+++ src/components/PasswordReset.tsx
@@ -3,7 +3,7 @@ import {
   View, Text, TextInput, TouchableOpacity, ActivityIndicator, Alert, StyleSheet 
 } from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
-import api from '../services/api';
+import api from '../../services/api'
 
 const PasswordReset: React.FC = () => {
   const route = useRoute();
