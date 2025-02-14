3e10e952583f199675b57008ee11365d080a2789
Ricky4412 <124883161+Ricky4412@users.noreply.github.com>
2025-02-14 18:48:27 +0000

    Update subscriptionService.ts

--- services/subscriptionService.ts
+++ services/subscriptionService.ts
@@ -1,6 +1,6 @@
 import api from './api';
 
-export const createSubscription = async (subscriptionData: { bookId: string; plan: string; price: number; duration: number }): Promise<any> => {
+export const createSubscription = async (subscriptionData: { bookId: string; plan: string; price: number; duration: number; mobileNumber: string; serviceProvider: string; accountName: string }): Promise<any> => {
   try {
     const response = await api.post('/subscriptions', subscriptionData);
     return response.data;
@@ -18,7 +18,7 @@ export const getUserSubscription = async (bookId: string): Promise<any> => {
   }
 };
 
-export const initializePayment = async (paymentData: { email: string; amount: number }): Promise<any> => {
+export const initializePayment = async (paymentData: { email: string; amount: number; mobileNumber: string; serviceProvider: string; accountName: string }): Promise<any> => {
   try {
     const response = await api.post('/subscriptions/pay', paymentData);
     return response.data;
